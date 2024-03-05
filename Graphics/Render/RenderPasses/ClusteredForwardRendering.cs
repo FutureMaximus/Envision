e87789e4f999083574b040595bd990883a003567
Ruben Jones <rubenjones734@hotmail.com>
2024-03-04 21:25:48 -0800

    Light culling and light max range implementation.

---
+++ Graphics/Render/RenderPasses/ClusteredForwardRendering.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Envision.Graphics.Render.RenderPasses;
+
+internal class ClusteredForwardRendering
+{
+}
--- Graphics/Shaders/Data/PBRLightData.cs
+++ Graphics/Shaders/Data/PBRLightData.cs
@@ -6,10 +6,17 @@ public struct PBRLightData : IShaderData
 {
     public readonly string Name => nameof(PBRLightData);
 
+    /// <summary>The color of the light.</summary>
     public Vector3 Color;
+    /// <summary>The intensity of the light this is multiplied with the attenuation factor.</summary>
     public float Intensity;
+    /// <summary>The maximum range before this light is culled.</summary>
+    public float MaxRange;
+    /// <summary>Constant attenuation factor.</summary>
     public float Constant;
+    /// <summary>Linear attenuation factor.</summary>
     public float Linear;
+    /// <summary>Quadratic attenuation factor.</summary>
     public float Quadratic;
 
     public PBRLightData(Vector3 color, float intensity, float constant, float linear, float quadratic)
